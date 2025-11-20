8c75b64dd838f8f2e4cc6725ccacfbbd37c76b88
Suryansh Varshney <118445319+weebsuryansh@users.noreply.github.com>
2025-11-20 22:02:20 +0530

    Initial commit

---
+++ src/main/java/org/example/odysseyeventapproval/OdysseyEventApprovalApplication.java
@@ -0,0 +1,13 @@
+package org.example.odysseyeventapproval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdysseyEventApprovalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdysseyEventApprovalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/odysseyeventapproval/OdysseyEventApprovalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.odysseyeventapproval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdysseyEventApprovalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
